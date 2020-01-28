c6b5aa8925d621b0dd5a3e3def1377745842fdd5
Andrzej <andrzejstudia@gmail.com>
2020-01-28 16:55:17 +0100

    Initial Commit

---
+++ src/main/java/com/darkflame/standardjpaoperations/StandardjpaoperationsApplication.java
@@ -0,0 +1,13 @@
+package com.darkflame.standardjpaoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardjpaoperationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StandardjpaoperationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/darkflame/standardjpaoperations/StandardjpaoperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkflame.standardjpaoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandardjpaoperationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
